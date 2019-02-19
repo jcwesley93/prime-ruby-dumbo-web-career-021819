61cb494fa7e415b1f9e145dcfa49f071b89d0dc4
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 10:46:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+  if n == 0 || 1
+    return false
+  elsif n < 0
+    return false
+  elsif for n in 2..(n-1)
+    if n % i == 0
+      return false
+  else return true
+    end
+  end
+ end
+end
