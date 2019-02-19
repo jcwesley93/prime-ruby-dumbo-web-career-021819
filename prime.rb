6619f0c5226925ac0c1dbe25ae01f554d5a67e45
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 10:55:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,29 +1,30 @@
-# def prime?(n)
-#   if n <= 1
-#     return false
-#   elsif n == 2
-#     return true
-#   elsif
-#     integers = 2...n
-#     integers.each do |int|
-#       if n % int == 0
-#         return false
-#   end
-# end
-#  end
-# end
-
-def prime?(number)
-  if number <= 1
+def prime?(n)
+  if n <= 1
     return false
-  elsif number == 2
+  elsif n == 2
     return true
-  end
-  testers = 2...number
-  testers.each do |tester|
-    if number % tester == 0
-      return false
+  elsif
+    integers = 2...n
+    integers.each do |int|
+      if n % int == 0
+        return false
+      end
     end
-  end
-  true
+    return true
+ end
 end
+
+# def prime?(number)
+#   if number <= 1
+#     return false
+#   elsif number == 2
+#     return true
+#   end
+#   testers = 2...number
+#   testers.each do |tester|
+#     if number % tester == 0
+#       return false
+#     end
+#   end
+#   true
+# end
