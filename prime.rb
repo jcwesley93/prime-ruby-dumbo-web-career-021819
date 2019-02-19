1d8d4787d604ac5e52584c4f125c08422d019259
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 10:56:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -14,17 +14,3 @@ def prime?(n)
  end
 end
 
-# def prime?(number)
-#   if number <= 1
-#     return false
-#   elsif number == 2
-#     return true
-#   end
-#   testers = 2...number
-#   testers.each do |tester|
-#     if number % tester == 0
-#       return false
-#     end
-#   end
-#   true
-# end
